4e8c7122769a1bec9df078dfa3ea149a3c424c7c
cktalala <boompetbank@gmail.com>
2026-06-09 16:46:22 +0700

    first commit

---
+++ src/components/UpdateTimestamp.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let date: Date;
+
+	let updatedText = '';
+
+	onMount(() => {
+		const [day, month, year, time] = new Date(date)
+			.toLocaleString('TH-th', {
+				dateStyle: 'medium',
+				timeStyle: 'short',
+			})
+			.split(' ');
+
+		updatedText = `วันที่ ${day} ${month} ${year.slice(2, 4)} เวลา ${time} น.`;
+	});
+</script>
+
+<p
+	class="body-01 opacity-50 {updatedText
+		? 'block'
+		: 'invisible'} {$$restProps.class}"
+>
+	อัปเดตข้อมูลล่าสุด {updatedText}
+</p>
